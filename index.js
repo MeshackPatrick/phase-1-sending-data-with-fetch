0226889a0e135759215a609ad9a2f6a415a5ac7a
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-26 20:29:16 +0300

    fetching data

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName,userEmail){
+    const userData={
+        name:userName,
+        email:userEmail
+    };
+    const configObj={
+        method:`POST`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+        body:JSON.stringify(userData)
+    };
+    return fetch(`http://localhost:3000/users`,configObj)
+        .then(response=>response.json())
+        .then(data=>{
+            const id=data.id
+            document.body.innerHTML+=`<p>${id}</p>`;
+        })
+    .catch(error=>{
+        document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+
+}
